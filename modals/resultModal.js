ac536aa1ae04207fdcc6706600665b8043208bf9
Binay shrestha <binayshrestha798@gmail.com>
2021-07-11 22:19:04 +0545

    result modal created

---
+++ modals/resultModal.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const resultSchema = new mongoose.Schema({
+  photoUrl: {
+    type: String,
+    required: [true, 'Must contain url of photo'],
+  },
+  name: {
+    type: String,
+    required: [true, 'Must contain name'],
+  },
+  grade: {
+    type: String,
+    required: [true, 'Must contain grade'],
+  },
+  batch: {
+    type: Number,
+    required: [true, 'Must contain batch'],
+  },
+});
+
+const Result = new mongoose.model('Result', resultSchema);
+module.exports = Result;
